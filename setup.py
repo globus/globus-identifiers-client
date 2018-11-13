fed4ced0d13eeabad1ec72fbbcfd17829198fd0b
Nickolaus D. Saint <nick@globus.org>
2018-11-13 08:25:54 -0800

    Fixed bugs when --no-local-server is invoked during login

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
     description='Globus Identifiers Service Client Command',
     include_package_data=True,
     packages=find_packages(),
-
+    install_requires=[
+        "globus-sdk>=1.4.0",
+        "six>=1.10.0,<2.0.0",
+    ],
     entry_points={
         'console_scripts': [
             ('globus-identifiers-client = identifiers_client:main')
