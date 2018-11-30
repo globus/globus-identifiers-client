4a9358f532a24877ed3ef86d4ccda9a9cda1662c
Jim Pruyne <pruyne@uchicago.edu>
2018-11-30 13:17:59 -0600

    move version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='globus-identifiers-client', version='0.1.0',
+    name='globus-identifiers-client', version='0.2.0',
     description='Globus Identifiers Service Client Command',
     include_package_data=True,
     packages=find_packages(),
